9b7e785108c7f3d705d7f8d13e60a47659d499dc
Hassan Masood <iamhassanmasood@gmail.com>
2023-01-20 15:51:40 +0500

    fix: saga configured

--- Counter.js
+++ Counter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) => (
   <div>
-    <button onClick={onIncrement}>Increment</button>{" "}
+    <button onClick={onIncrement}>Increment</button>
     <button onClick={onDecrement}>Decrement</button>
+    <button onClick={onIncrementAsync}>Increment Async 1 sec delay</button>
     <hr />
     <div>Count is: {value} times</div>
   </div>
@@ -14,6 +15,7 @@ Counter.propTypes = {
   value: PropTypes.number.isRequired,
   onIncrement: PropTypes.func.isRequired,
   onDecrement: PropTypes.func.isRequired,
+  onIncrementAsync: PropTypes.func.isRequired,
 };
 
 export default Counter;
